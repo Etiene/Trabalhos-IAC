54c39ce2747ba8863dc5bb9461dfcbdb3b18c327
Etiene da Cruz Dalcol <dalcol@etiene.net>
2012-06-09 02:31:44 -0300

    Adicionando o conversor de decimal pra binário

---
+++ dec-bin_conv.c
@@ -0,0 +1,57 @@
+//================================================================//
+//=====================  DEC-BIN CONV v1.0  ======================//
+//== Converte números naturais decimais para binários de 8 bits ==//
+//================================================================//
+// Por: Etiene da Cruz Dalcol. Mat 1211996. T 3WA. Junho de 2012  //
+// Para: Disciplina de Introdução a Arquitetura de Computadores   //
+//       ministrada por Luiz Fernando Seibel.                     //
+//================================================================//
+// Log de alterações:                                             //
+// 09/12/2012 - V1.0: Criação do programa.                        //
+//================================================================//
+
+#include <stdio.h>
+
+int ler_dec(int dec){
+	if(dec<0){
+		printf("Entrada: %d.\nERROR BRAIN 404!!! A entrada precisa ser um numero NATURAL.\n",dec);
+		return 1;
+	}
+	else if(dec>256){
+		printf("Entrada: %d.\nERROR OVERFLOW 666 FROM HELL!!! Impossivel armazenar um numero natural maior que 256 na base 10 dentro de 8 bits.\n",dec);
+		return 1;
+	}
+	return 0;
+}
+
+void dec_bin(int dec, int bin[]){
+	int i;
+	
+	for(i=7;i>=0;i--){
+		if((dec==0)||(dec%2==0))
+			bin[i]=0;
+		else 
+			bin[i]=1;
+		dec = dec/2;
+	}
+}
+
+int main(void){
+	FILE * fpB = fopen("res_dec-bin.txt","w");
+	int i,j,bin[8];
+	int decs[5]={0,-5,314,15,234};
+	
+	for(i=0;i<5;i++){
+		if(ler_dec(decs[i]))
+			fprintf(fpB,"%d: Entrada invalida.\n",decs[i]);
+		else {
+			fprintf(fpB,"%d: ",decs[i]);
+			dec_bin(decs[i],bin);
+			for(j=0;j<8;j++)
+				fprintf(fpB,"%d",bin[j]);
+			fprintf(fpB,"\n");
+		}
+	}
+	fclose(fpB);
+	return 0;
+}

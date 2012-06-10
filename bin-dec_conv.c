bac58527a711db0c499fb9561a9f47235c6a6229
Etiene da Cruz Dalcol <dalcol@etiene.net>
2012-06-10 18:50:12 -0300

    Não usar só entradas de teste no bin-dec

--- bin-dec_conv.c
+++ bin-dec_conv.c
@@ -1,5 +1,5 @@
 //================================================================//
-//=====================  BIN-DEC CONV v1.0  ======================//
+//=====================  BIN-DEC CONV v1.1  ======================//
 //== Converte números binários de 8 bits para naturais decimais ==//
 //================================================================//
 // Por: Etiene da Cruz Dalcol. Mat 1211996. T 3WA. Junho de 2012  //
@@ -8,13 +8,15 @@
 //================================================================//
 // Log de alterações:                                             //
 // 10/12/2012 - V1.0: Criação do programa.                        //
+// 10/12/2012 - V1.1: Adicionando opcao de escolher um nnumero e  //
+//                    não só usar as entrads de teste             //
 //================================================================//
 
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
 
-int ler_bin(char bin[]){
+int validar_bin(char bin[]){
 	int i;
 	if(strlen(bin)!=8){
 		printf("ERROR ??? !!! A entrada precisa ter OITO bits. \n");
@@ -26,6 +28,13 @@ int ler_bin(char bin[]){
 			return 1;
 		}
 	return 0;
+}
+
+int ler_bin(char bin[]){
+	printf("Digite um numero binario de 8 bits para converter para decimal natural: \n (Entradas maiores que 8 bits serao cortadas)\n");
+	scanf(" %s",bin);
+	if(validar_bin(bin))
+		ler_bin(bin);
 }
 
 int bin_dec(char bin[]){
@@ -40,14 +49,30 @@ int bin_dec(char bin[]){
 int main(void){
 	FILE * fpB = fopen("res_bin-dec.txt","w");
 	int i,dec;
-	char bins[5][10]={"1010","101010101","2312","01010010","10100001"};
+	char bin[9],c;
+	printf("i = Iniciar programa. t = Testar com varias entradas(output em .txt).\n");
+	scanf(" %c",&c);
+	if(c=='i')
+		while(1){
+			ler_bin(bin);
+			printf("Resultado: %d.\n",bin_dec(bin));
+			printf("Ler outro numero? s = Sim. n = Nao\n");
+			scanf(" %c",&c);
+			if(c!='s')
+				break;
+		}
+	else if(c=='t'){
+		char bins[7][10]={"1010","101010101","2312","01010010","10100001","01100011","00000110"};
 	
-	for(i=0;i<5;i++){
-		if(ler_bin(bins[i]))
-			fprintf(fpB,"%s: Entrada invalida.\n",bins[i]);
-		else
-			fprintf(fpB,"%s: %d\n",bins[i],bin_dec(bins[i]));
-	}
+		for(i=0;i<7;i++){
+			if(validar_bin(bins[i]))
+				fprintf(fpB,"%s: Entrada invalida.\n",bins[i]);
+			else
+				fprintf(fpB,"%s: %d\n",bins[i],bin_dec(bins[i]));
+		}
+	} else
+		printf("Opcao invalida.\n");
 	fclose(fpB);
+	system("pause");
 	return 0;
 }
